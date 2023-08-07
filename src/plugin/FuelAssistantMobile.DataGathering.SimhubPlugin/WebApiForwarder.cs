d2656ea8999fce27d931eeef362606cabcd6985b
MacReiben <macreiben@gmail.com>
2023-08-07 15:55:30 +0200

    slicing things to make life easiest.

--- src/plugin/FuelAssistantMobile.DataGathering.SimhubPlugin/WebApiForwarder.cs
+++ src/plugin/FuelAssistantMobile.DataGathering.SimhubPlugin/WebApiForwarder.cs
@@ -1,5 +1,6 @@
 ﻿using FuelAssistantMobile.DataGathering.SimhubPlugin.Aggregations;
 using FuelAssistantMobile.DataGathering.SimhubPlugin.Logging;
+using FuelAssistantMobile.DataGathering.SimhubPlugin.Repositories;
 using GameReaderCommon;
 using SimHub.Plugins;
 using System;
@@ -24,6 +25,8 @@ namespace FuelAssistantMobile.DataGathering.SimhubPlugin
         private Timer _postTimer;
         private Timer _autoReactivate;
         private ILiveAggregator _liveAggregator;
+        private IStagingDataRepository _dataRepository;
+        private readonly IStagingDataRepository dataRepository;
         private ILogger _logger;
         private PluginManager _pluginManager;
 
@@ -31,12 +34,18 @@ namespace FuelAssistantMobile.DataGathering.SimhubPlugin
         private bool _notifiedStop = false;
 
         public WebApiForwarder()
-            : this(new SimhubLogger(), new LiveAggregator())
+            : this(
+                  new SimhubLogger(),
+                  new LiveAggregator(),
+                  new FamRemoteRepository())
         {
 
         }
 
-        public WebApiForwarder(ILogger logger, ILiveAggregator aggregator)
+        public WebApiForwarder(
+            ILogger logger,
+            ILiveAggregator aggregator,
+            IStagingDataRepository dataRepository)
         {
             _httpClient = new HttpClient();
 
@@ -48,20 +57,14 @@ namespace FuelAssistantMobile.DataGathering.SimhubPlugin
 
             _liveAggregator = aggregator;
 
+            _dataRepository = dataRepository;
             _logger = logger;
         }
 
-        private void AutoReactivate(object sender, ElapsedEventArgs e)
-        {
-            if(_notifiedStop && _internalErrorCount >= 3)
-            {
-                _logger.Info("Trying to restart plugin after errors ...");
-
-                Reset();
-            }
-        }
-
         public PluginManager PluginManager { set => _pluginManager = value; }
+
+        // ===========================================================
+        // ===========================================================
 
         public void DataUpdate(PluginManager pluginManager, ref GameData data)
         {
@@ -98,6 +101,9 @@ namespace FuelAssistantMobile.DataGathering.SimhubPlugin
             _liveAggregator.Clear(); // just in case ... 
         }
 
+        // ===========================================================
+        // ===========================================================
+
         private void Reset()
         {
             _liveAggregator.Clear();
@@ -108,66 +114,78 @@ namespace FuelAssistantMobile.DataGathering.SimhubPlugin
             _internalErrorCount = 0;
         }
 
-
         // THOUGHT: this one should be moved in a dedicated class.
         private async void PostData(object sender, ElapsedEventArgs e)
         {
             // THOUGHT: check game status before doing anything. If it is not running. Then do nothing.
-            string jsonData = null;
+            if (ShouldStopTimer())
+            {
+                try
+                {
+                    _postTimer.Stop();
+                }
+                catch { }
+
+                _logger.Error("WebAPI post stoped.");
+                _notifiedStop = true;
+                return;
+            }
+
+            if (ShouldNotifyRetrying())
+            {
+                _logger.Warn($"Retrying to contact API - error count is [{_internalErrorCount}]");
+            }
+
+            if (!_liveAggregator.IsDirty)
+            {
+                return;
+            }
+
+            // Replace the following lines with your own logic to get the data you want to send
+            var dataToSend = new
+            {
+                data = _liveAggregator.AsData()
+            };
+
             try
             {
-                if (_internalErrorCount >= 3)
-                {
-                    try
-                    {
-                        _postTimer.Stop();
-                    }
-                    catch { }
-
-                    _logger.Error("WebAPI post stoped.");
-                    _notifiedStop = true;
-                    return;
-                }
-
-                if (_internalErrorCount > 0 && _internalErrorCount < 3)
-                {
-                    _logger.Warn($"Retrying to contact API - error count is [{_internalErrorCount}]");
-                }
-
-                if (!_liveAggregator.IsDirty)
-                {
-                    return;
-                }
-
-                // Replace the following lines with your own logic to get the data you want to send
-                var dataToSend = new
-                {
-                    data = _liveAggregator.AsData()
-                };
-
-                // Convert the data to JSON
-                jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(dataToSend);
-                StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-
-                // Post the data to the WebAPI
-                var response = await _httpClient.PostAsync(WebApiUrl, content);
-
-                if (response.StatusCode != HttpStatusCode.OK)
-                {
-                    _internalErrorCount++;
-
-                    _logger.Error($"Issue during posting [{WebApiUrl}] - [{_internalErrorCount}] error count.");
-                    _logger.Error($"API failed returned code is not OK - [{response.StatusCode}]");
-                }
+                await _dataRepository.SendAsync(dataToSend);
             }
-            catch (Exception ex)
+            catch (ErrorWhenSendDataException ex)
             {
                 _internalErrorCount++;
 
-                _logger.Error($"Issue during posting [{WebApiUrl}] - [{_internalErrorCount}] error count.");
+                _logger.Error($"Issue during posting [{ex.WebApiUrl}] - [{_internalErrorCount}] error count.");
                 _logger.Error("Posted data is:");
-                _logger.Error(jsonData);
+                _logger.Error(ex.JsonData);
                 _logger.Error($"Exception is:", ex);
+            }
+            catch (StatusCodeNotOkException ex)
+            {
+                _internalErrorCount++;
+
+                _logger.Error($"Issue during posting [{ex.WebApiUrl}] - [{_internalErrorCount}] error count.");
+                _logger.Error($"API failed returned code is not OK - [{ex.StatusCode}]");
+            }
+        }
+
+        private bool ShouldStopTimer()
+        {
+            return _internalErrorCount >= 3;
+        }
+
+        private bool ShouldNotifyRetrying()
+        {
+            return _internalErrorCount > 0 && _internalErrorCount < 3;
+        }
+
+        private void AutoReactivate(object sender, ElapsedEventArgs e)
+        {
+            if (_notifiedStop && _internalErrorCount >= 3)
+            {
+                _logger.Info("Trying to restart plugin after errors ...");
+
+                Reset();
             }
         }
     }
