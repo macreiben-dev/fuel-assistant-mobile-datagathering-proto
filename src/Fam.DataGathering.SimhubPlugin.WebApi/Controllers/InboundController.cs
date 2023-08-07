b17f6d39a7885b868c1a1c6adfbdeaa0d6ac84bb
MacReiben <macreiben@gmail.com>
2023-08-07 11:13:06 +0200

    api is responding correctly.

---
+++ src/Fam.DataGathering.SimhubPlugin.WebApi/Controllers/InboundController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Fam.DataGathering.SimhubPlugin.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InboundController
+    {
+        [HttpPost]
+        public void Post([FromBody] object data)
+        {
+            Trace.WriteLine(data.ToString());
+        }
+    }
+}
